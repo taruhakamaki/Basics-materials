dc9cf544de733fd242b495082ced684708b4c78c
Taru <Taru.Hakamaki@tuni.fi>
2024-04-28 20:45:17 +0300

    modify

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,4 +1,10 @@
-print("{}: {}".format(key, value))
+def debug_print(debug_msg=None, **kwargs):
+
+    if debug_msg:
+        print(debug_msg)
+
+    for key, value in kwargs.items():
+        print("{}: {}".format(key, value))
 
 
 def mergesort(array):
@@ -38,7 +44,16 @@ def merge(left, right):
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
 
-    quit(1)
+    input_list = input_str.split(",")
+    debug_print(input_list=input_list)
+
+    value_list = []
+    for x in input_list:
+        try:
+            value_list.append(int(x))
+        except ValueError as err:
+            print("Invalid input.")
+            quit(1)
 
     debug_print(value_list=value_list)
 
